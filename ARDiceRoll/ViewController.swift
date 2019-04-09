3825527ec39f36287472a6456fb7cd8cd7d6dcb1
Kevin Joseph Mangulabnan <44663984+Wattholm@users.noreply.github.com>
2019-04-09 17:14:59 +0800

    Add sphere (earth) and cube (dice) nodes.  Run AR configuration based on device specs.

--- ARDiceRoll/ViewController.swift
+++ ARDiceRoll/ViewController.swift
@@ -20,24 +20,62 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // Node for Dice
+        let diceGeo = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.005)
+        let diceMat = SCNMaterial()
+        diceMat.diffuse.contents = UIColor.blue
+        diceGeo.materials = [diceMat]
+        let diceNode = SCNNode()
+        diceNode.position = SCNVector3(x:0, y:-1, z:-0.5)
+        // Give the node a geometry of cube
+        diceNode.geometry = diceGeo
+        
+        // Node for Earth
+        let earthGeo = SCNSphere(radius: 0.2)
+        let earthMat = SCNMaterial()
+        earthMat.diffuse.contents = UIImage(named: "art.scnassets/earth_day_2k.jpg")
+        earthGeo.materials = [earthMat]
+        let earthNode = SCNNode()
+        earthNode.position = SCNVector3(x:1, y:0.1, z:-0.7)
+        // Give the node a geometry of cube
+        earthNode.geometry = earthGeo
+
+        sceneView.scene.rootNode.addChildNode(diceNode)
+        sceneView.scene.rootNode.addChildNode(earthNode)
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+        
+        //// Code from default AR Project
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.scene = scene
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        print("Session is supported = \(ARConfiguration.isSupported)")
+        print("World Tracking is supported = \(ARWorldTrackingConfiguration.isSupported)")
 
-        // Run the view's session
-        sceneView.session.run(configuration)
+        if ARWorldTrackingConfiguration.isSupported {
+            // Use Configuration Compatible with AR9+ Chips
+            let configuration = ARWorldTrackingConfiguration()
+            // Run the view's session
+            sceneView.session.run(configuration)
+        } else {
+            // Use Configuration for older models with processors lower than AR9
+            let configuration = AROrientationTrackingConfiguration()
+            // Run the view's session
+            sceneView.session.run(configuration)
+        }
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -46,6 +84,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // MARK: - ARSCNViewDelegate
     
